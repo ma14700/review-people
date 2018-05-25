27795f2e226962a4abf521c79158060e2365a697
mage <244841522@qq.com>
2018-05-25 18:05:48 +0800

    开始home页面

---
+++ src/envconfig/envconfig.js
@@ -0,0 +1,11 @@
+//全局配置
+
+let baseURL;
+let imgUrl = '//elm.cangdu.org/img/';
+if(process.env.NODE_ENV === 'development'){
+    baseURL = '//api.cangdu.org'
+}else{
+    baseURL = '//api.cangdu.org';
+}
+
+export default {imgUrl,baseURL};
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,14 @@
 import React, { component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+// Immutable数据就是一旦创建，就不能更改的数据。每当对Immutable对象进行修改的时候，就会返回一个新的Immutable对象，以此来保证数据的不可变,
 import { is, fromJS } from 'immutable';
+//利用prop-types第三方库对组件的props中的变量进行类型检测
+import PropTypes from 'prop-types'
+import API from '../../api/api';
+import envconfig from '../../envconfig/envconfig';
+
+
 
 class Home extends React.Component{
     render(){
